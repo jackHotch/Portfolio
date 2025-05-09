6624bd0031dc6bafed16caae371d435d9d1ecd52
jackHotch <jhotchkiss2021@gmail.com>
2025-05-09 16:32:48 -0400

    finished resume button

--- src/components/ui/resume.tsx
+++ src/components/ui/resume.tsx
@@ -1,7 +1,17 @@
+import { TbLocationFilled } from 'react-icons/tb'
+
 export const Resume = () => {
+  const handleOpenPdf = () => {
+    const pdfUrl = '/jack_hotchkiss_resume.pdf' // could also be an external URL
+    window.open(pdfUrl, '_blank')
+  }
   return (
-    <div className='py-3 px-5 absolute top-4 right-18 z-10  rounded-xl bg-slate-400/15'>
+    <button
+      onClick={handleOpenPdf}
+      className='py-3 px-5 absolute top-4 right-18 z-10 border-1 border-accent-100 rounded-xl bg-slate-400/15 flex-center gap-2'
+    >
       Resume
-    </div>
+      <TbLocationFilled className='text-accent-100' />
+    </button>
   )
 }
