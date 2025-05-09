d8ee8094156aac5c4380bd60a878160c018dd481
jackHotch <jhotchkiss2021@gmail.com>
2025-05-08 21:20:57 -0400

    created basic resume button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 'use client'
 
 import { Hero } from '@/components/hero'
-import { Logo } from '@/components/logo'
+import { Logo } from '@/components/ui/logo'
 import { Navbar } from '@/components/navbar'
 import { Skills } from '@/components/skills'
+import { Resume } from '@/components/ui/resume'
 
 export default function Home() {
   return (
     <main className='mx-auto flex flex-col gap-3 min-h-screen'>
       <Logo />
+      <Resume />
       <Navbar />
       <Hero />
       <Skills />
---
+++ src/components/ui/resume.tsx
@@ -0,0 +1,7 @@
+export const Resume = () => {
+  return (
+    <div className='py-3 px-5 absolute top-4 right-18 z-10  rounded-xl bg-slate-400/15'>
+      Resume
+    </div>
+  )
+}
